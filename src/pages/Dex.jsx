493e9848a87ec331b0c0f2f8def99ee8bda8ef10
JDY <wjsehdud@nate.com>
2025-02-21 17:47:22 +0900

    포켓몬 추가 기능에 중복 및 최대 개수 제한 로직 추가

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -7,6 +7,14 @@ const Dex = () => {
   const [selectedPokemon, setSelectedPokemon] = useState([]);
 
   const addPokemon = (pokemon) => {
+    if (selectedPokemon.find((e) => e.id === pokemon.id)) {
+      return alert("이미 추가된 포켓몬입니다!");
+    }
+
+    if (selectedPokemon.length >= 6) {
+      return alert("포켓몬은 최대 6마리만 소유할 수 있습니다!");
+    }
+
     setSelectedPokemon([...selectedPokemon, pokemon]);
   };
 
