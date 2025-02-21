334560cfabe7009b07b87fd8dbab634a3c4c4791
JDY <wjsehdud@nate.com>
2025-02-21 18:57:45 +0900

    포켓몬 상세 페이지 수정: 불필요한 Link 임포트 제거

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import MOCK_DATA from "../mock";
 
 const PokemonDetail = () => {
