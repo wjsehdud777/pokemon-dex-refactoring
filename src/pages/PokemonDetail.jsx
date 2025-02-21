dbccccde02e1e1dbd93111c8faea787ff510d4c1
JDY <wjsehdud@nate.com>
2025-02-21 18:18:25 +0900

    포켓몬 상세 페이지 개선: 포켓몬이 없을 경우 에러 메시지 및 뒤로 가기 버튼 추가

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -1,19 +1,27 @@
-import { Link, Navigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import MOCK_DATA from "../mock";
 
 const PokemonDetail = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const pokemon = MOCK_DATA.find((pokemon) => pokemon.id === Number(id));
 
-  console.log(pokemon);
+  if (!pokemon) {
+    return (
+      <div>
+        <p>포켓몬을 찾을 수 없습니다!</p>
+        <button onClick={() => navigate(-1)}>뒤로 가기</button>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <button onClick={() => Navigate(-1)}>뒤로 가기</button>
       <h1>{pokemon.korean_name}</h1>
       <img src={pokemon.img_url} alt={pokemon.korean_name} />
       <p>Type: {pokemon.types.join(", ")}</p>
       <p>{pokemon.description}</p>
+      <button onClick={() => navigate(-1)}>뒤로 가기</button>
     </div>
   );
 };
