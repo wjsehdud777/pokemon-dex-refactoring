ff6efaa56ea40cb328f540f2295283e899ddd16f
JDY <wjsehdud@nate.com>
2025-02-24 10:54:58 +0900

    홈 페이지 개선: 버튼 스타일링 추가 및 styled-components 사용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to Pokemon Dex</h1>
       <Link to={"/dex"}>
-        <button>포켓몬 도감 시작하기</button>
+        <StHomeButton>포켓몬 도감 시작하기</StHomeButton>
       </Link>
     </div>
   );
 };
+
+const StHomeButton = styled.button`
+  background-color: #ff0000;
+  border: none;
+  border-radius: 5px;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 16px;
+  padding: 10px 20px;
+  text-transform: uppercase;
+
+  &:hover {
+    transition: background-color 0.5s ease;
+    background-color: rgb(114, 0, 0);
+  }
+`;
 
 export default Home;
